739c456c508882a302afa28303070c93fb52a284
asimrauf101 <asim.rauf@ropstam.com>
2023-11-24 14:54:42 +0500

    mobile menu icon to the right

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ const Search = (): JSX.Element => {
 	};
 
 	return (
-		<div className='lg:w-[300px] xl:w-[500px] w-[290px] lg:max-w-screen-lg max-w-2xl'>
+		<div className='lg:w-[300px] xl:w-[500px] w-[230px] lg:max-w-screen-lg max-w-2xl'>
 			<SearchBar
 				placeholder='Ask a question...'
 				onSearch={handleSearch}
