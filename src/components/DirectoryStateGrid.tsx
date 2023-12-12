0295e5c319afd42da0a979093e7eca70d99569c2
asimrauf101 <asim.rauf@ropstam.com>
2023-12-12 18:10:58 +0500

    issue fixed for spacing

--- src/components/DirectoryStateGrid.tsx
+++ src/components/DirectoryStateGrid.tsx
@@ -34,7 +34,7 @@ const DirectoryStateGrid = ({
 				<a
 					key='uRL'
 					href={relativePrefixToRoot + child.slug}
-					className='font-semibold text-[18px] md:text-s  lg:text-[18px] text-typography-link hover:underline  '>
+					className='font-semibold text-[18px] small:text-[14px] md:text-s  lg:text-[18px] text-typography-link hover:underline  '>
 					{child.name}{' '}
 				</a>
 				<span className='text-typography-lightGray text-xs font-normal'>
@@ -47,7 +47,7 @@ const DirectoryStateGrid = ({
 		<>
 			<div className='space-y-10 mt-6 '>
 				{directoryChildren && (
-					<div className='grid  grid-cols-2 w-max max-w-lg   lg:grid-cols-2 xl:grid-cols-3 md:gap-x-2  md:grid-cols-3 gap-4   xl:gap-x-[9rem] gap-y-[3rem]'>
+					<div className='grid  grid-cols-2 w-max max-w-lg   lg:grid-cols-2 xl:grid-cols-3 md:gap-x-2  md:grid-cols-3 gap-6  small:gap-x-[1.5rem] small:gap-y-[1rem]   xl:gap-x-[9rem] gap-y-[3rem]'>
 						{childrenDivs}
 					</div>
 				)}
