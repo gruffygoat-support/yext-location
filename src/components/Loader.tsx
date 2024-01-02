28f967d1472674e28dbf586108fef31cf2edba4c
asimrauf101 <asim.rauf@ropstam.com>
2024-01-02 15:04:35 +0500

    fix: loader issue

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -7,9 +7,11 @@ const Loader = () => {
 		<>
 			<div
 				style={{
-					position: 'absolute',
+					position: 'fixed',
 					top: '50%',
 					left: '50%',
+					transform: 'translate(-50%, -50%)',
+					zIndex: '999', // Ensure the loader is on top of other elements
 				}}>
 				<TailSpin
 					height='100'
